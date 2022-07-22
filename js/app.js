fd712a6a58b86fb8b441623e2429994f4fbcb5c5
Olayinka Ogunwemimo <mimoyinka@gmail.com>
2022-07-22 13:07:08 +0100

    Another one

--- js/app.js
+++ js/app.js
@@ -1,50 +1,53 @@
 let nav = document.querySelector("#navbar__list");
 
-const ACTIVE_CLASS = "active";
+const ACTIVE_CLASS = "in-view";
 
-function observeSections(){
-    const inViewHandler = (entries) => {
-        entries.forEach((entry) => {
-            const {target, isIntersecting} = entry;
-          if (isIntersecting) {
-            target.classList.add(ACTIVE_CLASS);
-            Array.from(document.querySelectorAll(".menu__link"))
-                .filter((item) => item.hash.includes(target.id)).pop().classList.add(ACTIVE_CLASS);
-          } else {
-            entry.target.classList.remove(ACTIVE_CLASS);
-            Array.from(document.querySelectorAll(".menu__link"))
-                .filter((item) => item.hash.includes(target.id)).pop().classList.remove(ACTIVE_CLASS);
-          }
-        });
-      };
-    const observer = new IntersectionObserver(inViewHandler, {
-        threshold: 0.3
-    });
-    let targets = document.querySelectorAll("section");
-    targets.forEach((target) => observer.observe(target));
+function observeSections() {
+  const inViewHandler = (entries) => {
+    const lis = Array.from(document.querySelectorAll(".menu__link"));
+    lis.length &&
+      entries.forEach((entry) => {
+        const { target, isIntersecting } = entry;
+        if (isIntersecting) {
+          target.classList.add(ACTIVE_CLASS);
+          lis
+            .filter((item) => item.hash.includes(target.id))
+            .pop()
+            .classList.add(ACTIVE_CLASS);
+        } else {
+          entry.target.classList.remove(ACTIVE_CLASS);
+          lis
+            .filter((item) => item.hash.includes(target.id))
+            .pop()
+            .classList.remove(ACTIVE_CLASS);
+        }
+      });
+  };
+  const observer = new IntersectionObserver(inViewHandler, {
+    threshold: 0.3
+  });
+  let targets = document.querySelectorAll("section");
+  targets.forEach((target) => observer.observe(target));
 }
 
 function buildNav() {
-    let sections = document.querySelectorAll("section");
-    let fragment = document.createDocumentFragment();
-    Array.from(sections).forEach((section) => {
-        //  2. Build Nav
-        let li = document.createElement("li");
-        li.innerHTML = `<a href=#${section.id} class="menu__link">${section.dataset["nav"]}</a>`;
-        const anchor = li.querySelector("a");
-        anchor.addEventListener("click", (e) => {
-            e.preventDefault();
-            section.scrollIntoView({ behavior: "smooth" });
-        });
-        fragment.appendChild(li);
+  let sections = document.querySelectorAll("section");
+  let fragment = document.createDocumentFragment();
+  Array.from(sections).forEach((section) => {
+    //  2. Build Nav
+    let li = document.createElement("li");
+    li.innerHTML = `<a href=#${section.id} class="menu__link">${section.dataset["nav"]}</a>`;
+    const anchor = li.querySelector("a");
+    anchor.addEventListener("click", (e) => {
+      e.preventDefault();
+      section.scrollIntoView({ behavior: "smooth" });
     });
-    nav.appendChild(fragment);
+    fragment.appendChild(li);
+  });
+  nav.appendChild(fragment);
 }
 
-document.addEventListener("readystatechange", function main({
-  target: { readyState }
-}) {
-  if (readyState === "complete") {
-    buildNav(); observeSections();
-  }
-});
+document.onload = (function () {
+  buildNav();
+  observeSections();
+})();
